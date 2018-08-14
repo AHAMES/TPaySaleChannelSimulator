9599615a7b14a26c7e58e0f92987348d1614a775
Ahmed ElSabbagh <Ahmed.ElSabbagh@tpay.me>
2018-08-14 11:36:33 +0200

    initial layout, subject to change

---
+++ TPaySaleChannelSimulator/Controllers/OperatorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TPaySaleChannelSimulator.Models;
+
+namespace TPaySaleChannelSimulator.Controllers
+{
+    public class OperatorController : Controller
+    {
+        // GET: Operator
+        public ActionResult Index()
+        {
+            var operators = new List<Operator>
+            {
+                new Operator
+                {
+                    name="Vodafone",
+                    Id=1
+                },
+                new Operator
+                {
+                    name="Etisalat",
+                    Id=2
+                },
+                new Operator
+                {
+                    name="Orange",
+                    Id=3
+                },
+                new Operator
+                {
+                    name="WE",
+                    Id=4
+                }
+            };
+            return View(operators);
+        }
+    }
+}
